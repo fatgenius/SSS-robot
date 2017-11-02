b78e3e1c598eda375a8227528702bb12c3b97eaa
fatgenius <dbtlive@gmail.com>
2017-11-02 10:02:58 +0100

    Add files via upload
    The infrared temp sensor

---
+++ readTemperature.ino
@@ -0,0 +1,45 @@
+/*
+ *IR SENSOR FOR MEASURE HOTSPORS IN CONCREAT
+ *
+ */
+
+#include "IRTemp.h"
+
+static const byte PIN_DATA    = 2; // Choose any pins you like for these
+static const byte PIN_CLOCK   = 3;
+static const byte PIN_ACQUIRE = 4;
+
+static const TempUnit SCALE=CELSIUS;  // Options are CELSIUS, FAHRENHEIT
+
+IRTemp irTemp(PIN_ACQUIRE, PIN_CLOCK, PIN_DATA);
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("The ");
+}
+
+void loop(void) {
+  float irTemperature = irTemp.getIRTemperature(SCALE);
+  printTemperature("IR", irTemperature);
+
+  float ambientTemperature = irTemp.getAmbientTemperature(SCALE);
+  printTemperature("Ambient", ambientTemperature);
+
+  delay(1000);
+}
+
+void printTemperature(
+  char  *type,
+  float  temperature) {
+
+  Serial.print(type);
+  Serial.print(" temperature: ");
+
+  if (isnan(temperature)) {
+    Serial.println("Failed");
+  }
+  else {
+    Serial.print(temperature);
+    Serial.println(SCALE == FAHRENHEIT  ?  " F"  :  " C");
+  }
+}
